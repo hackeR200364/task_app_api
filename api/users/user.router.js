48f24b3425fe2f91beed98066a7436129d501bfc
Rupam <rupamkarmakar1238@gmail.com>
2023-03-06 02:20:36 +0530

    TAA-2 committed

---
+++ api/users/user.router.js
@@ -0,0 +1,9 @@
+const { createUser, login } = require("./user.controller");
+const { checkToken } = require("../../auth/token_validation");
+
+const router = require("express").Router();
+
+router.post("/create", createUser);
+router.post("/login",checkToken, login);
+
+module.exports = router;
