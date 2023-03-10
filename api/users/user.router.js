649f4fb00da1a243a970fac8bb9ee40530c6c52a
Rupam Karmakar <86157946+hackeR200364@users.noreply.github.com>
2023-03-10 18:48:42 +0530

    remove checkToken from login

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -9,7 +9,7 @@ const { checkToken } = require("../../auth/token_validation");
 const router = require("express").Router();
 
 router.post("/create", createUser);
-router.post("/login", checkToken, login);
+router.post("/login", login);
 router.post("/googleLogin", googleLogin);
 router.get("/forgotPass/:usrEmail", forgotPass);
 
